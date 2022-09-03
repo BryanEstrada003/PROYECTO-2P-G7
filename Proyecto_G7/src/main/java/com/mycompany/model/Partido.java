f17963ffcba7312449698981b5d310415674043e
TingoCarlos08 <ctingo@espol.edu.ec>
2022-09-03 02:00:28 -0500

    Avance de la ventana partido

--- Proyecto_G7/src/main/java/com/mycompany/model/Partido.java
+++ Proyecto_G7/src/main/java/com/mycompany/model/Partido.java
@@ -1,6 +1,5 @@
 package com.mycompany.model;
 
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -12,12 +11,12 @@ import java.util.Arrays;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author USER
  */
 public class Partido {
+
     private String year;
     private String dateTime;
     private String stage; //Fases
@@ -38,124 +37,337 @@ public class Partido {
     private String matchID;
     private String homeTeamInitials;
     private String awayTeamInitials;
-     
-    
+
+    public Partido(String year, String dateTime, String stage, String stadium, String city, String homeTN, String homeTG, String awayTG, String awayTN, String winConditions, String attendance, String halfTimeHomeGoals, String halfTimeAwayGoals, String referee, String assistant1, String assistant2, String roundID, String matchID, String homeTeamInitials, String awayTeamInitials) {
+        this.year = year;
+        this.dateTime = dateTime;
+        this.stage = stage;
+        this.stadium = stadium;
+        this.city = city;
+        this.homeTN = homeTN;
+        this.homeTG = homeTG;
+        this.awayTG = awayTG;
+        this.awayTN = awayTN;
+        this.winConditions = winConditions;
+        this.attendance = attendance;
+        this.halfTimeHomeGoals = halfTimeHomeGoals;
+        this.halfTimeAwayGoals = halfTimeAwayGoals;
+        this.referee = referee;
+        this.assistant1 = assistant1;
+        this.assistant2 = assistant2;
+        this.roundID = roundID;
+        this.matchID = matchID;
+        this.homeTeamInitials = homeTeamInitials;
+        this.awayTeamInitials = awayTeamInitials;
+    }
+
+//Getters
+    public String getYear() {
+        return year;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public String getStage() {
+        return stage;
+    }
+
+    public String getStadium() {
+        return stadium;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getHomeTN() {
+        return homeTN;
+    }
+
+    public String getHomeTG() {
+        return homeTG;
+    }
+
+    public String getAwayTG() {
+        return awayTG;
+    }
+
+    public String getAwayTN() {
+        return awayTN;
+    }
+
+    public String getWinConditions() {
+        return winConditions;
+    }
+
+    public String getAttendance() {
+        return attendance;
+    }
+
+    public String getHalfTimeHomeGoals() {
+        return halfTimeHomeGoals;
+    }
+
+    public String getHalfTimeAwayGoals() {
+        return halfTimeAwayGoals;
+    }
+
+    public String getReferee() {
+        return referee;
+    }
+
+    public String getAssistant1() {
+        return assistant1;
+    }
+
+    public String getAssistant2() {
+        return assistant2;
+    }
+
+    public String getRoundID() {
+        return roundID;
+    }
+
+    public String getMatchID() {
+        return matchID;
+    }
+
+    public String getHomeTeamInitials() {
+        return homeTeamInitials;
+    }
+
+    public String getAwayTeamInitials() {
+        return awayTeamInitials;
+    }
+
+    //Setters
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setStage(String stage) {
+        this.stage = stage;
+    }
+
+    public void setStadium(String stadium) {
+        this.stadium = stadium;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setHomeTN(String homeTN) {
+        this.homeTN = homeTN;
+    }
+
+    public void setHomeTG(String homeTG) {
+        this.homeTG = homeTG;
+    }
+
+    public void setAwayTG(String awayTG) {
+        this.awayTG = awayTG;
+    }
+
+    public void setAwayTN(String awayTN) {
+        this.awayTN = awayTN;
+    }
+
+    public void setWinConditions(String winConditions) {
+        this.winConditions = winConditions;
+    }
+
+    public void setAttendance(String attendance) {
+        this.attendance = attendance;
+    }
+
+    public void setHalfTimeHomeGoals(String halfTimeHomeGoals) {
+        this.halfTimeHomeGoals = halfTimeHomeGoals;
+    }
+
+    public void setHalfTimeAwayGoals(String halfTimeAwayGoals) {
+        this.halfTimeAwayGoals = halfTimeAwayGoals;
+    }
+
+    public void setReferee(String referee) {
+        this.referee = referee;
+    }
+
+    public void setAssistant1(String assistant1) {
+        this.assistant1 = assistant1;
+    }
+
+    public void setAssistant2(String assistant2) {
+        this.assistant2 = assistant2;
+    }
+
+    public void setRoundID(String roundID) {
+        this.roundID = roundID;
+    }
+
+    public void setMatchID(String matchID) {
+        this.matchID = matchID;
+    }
+
+    public void setHomeTeamInitials(String homeTeamInitials) {
+        this.homeTeamInitials = homeTeamInitials;
+    }
+
+    public void setAwayTeamInitials(String awayTeamInitials) {
+        this.awayTeamInitials = awayTeamInitials;
+    }
+
+    @Override
+    public String toString() {
+        return "Partido{" + "year=" + year + ", dateTime=" + dateTime + ", stage=" + stage + ", stadium=" + stadium + ", city=" + city + ", homeTN=" + homeTN + ", homeTG=" + homeTG + ", awayTG=" + awayTG + ", awayTN=" + awayTN + ", winConditions=" + winConditions + ", attendance=" + attendance + ", halfTimeHomeGoals=" + halfTimeHomeGoals + ", halfTimeAwayGoals=" + halfTimeAwayGoals + ", referee=" + referee + ", assistant1=" + assistant1 + ", assistant2=" + assistant2 + ", roundID=" + roundID + ", matchID=" + matchID + ", homeTeamInitials=" + homeTeamInitials + ", awayTeamInitials=" + awayTeamInitials + '}';
+    }
+
     //ListaFase
-    public static ArrayList<String> listaFase(String nombreArchivo){
+    public static ArrayList<String> listaFase(String nombreArchivo) {
         ArrayList<String> grupoFase = new ArrayList<>();
-        
-         try(BufferedReader bf=new BufferedReader(new FileReader(nombreArchivo))){
+
+        try ( BufferedReader bf = new BufferedReader(new FileReader(nombreArchivo))) {
             String linea;
-            grupoFase.add("Group");  
-            while((linea = bf.readLine())!= null){
+            grupoFase.add("Group");
+            while ((linea = bf.readLine()) != null) {
                 String datos[] = linea.split("\\|");
                 //DatoFase
-                
-                if(!(grupoFase.contains(datos[2])) && !"Stage".equals(datos[2]) && !datos[2].startsWith("G")){
-                                      
+
+                if (!(grupoFase.contains(datos[2])) && !"Stage".equals(datos[2]) && !datos[2].startsWith("G")) {
+
                     grupoFase.add(datos[2]);
-              
+
                 }
-                
+
             }
-            
-        }catch(IOException ioe){
+
+        } catch (IOException ioe) {
             System.out.println("Ocurrió un problema con el aplicativo");
-            
+
         }
         return grupoFase;
-        
-        
+
     }
-    
+
     //listaGrupo
-    public static ArrayList<String> listaGrupos(String nombreArchivo){
+    public static ArrayList<String> listaGrupos(String nombreArchivo) {
         ArrayList<String> grupoLista = new ArrayList<>();
-        
-         try(BufferedReader bf=new BufferedReader(new FileReader(nombreArchivo))){
+
+        try ( BufferedReader bf = new BufferedReader(new FileReader(nombreArchivo))) {
             String linea;
-            
-            while((linea = bf.readLine())!= null){
+
+            while ((linea = bf.readLine()) != null) {
 //                System.out.println(linea);
                 String datos[] = linea.split("\\|");
                 //DatoFase
-                
-                if(!(grupoLista.contains(datos[2])) && !"Stage".equals(datos[2]) && datos[2].startsWith("G")){
-                    
+
+                if (!(grupoLista.contains(datos[2])) && !"Stage".equals(datos[2]) && datos[2].startsWith("G")) {
+
                     grupoLista.add(datos[2]);
-              
+
                 }
-                
+
             }
-            
-        }catch(IOException ioe){
+
+        } catch (IOException ioe) {
             System.out.println("Ocurrió un problema con el aplicativo");
-            
+
         }
         return grupoLista;
-    }          
-        
+    }
+
     //Lectura de archivo y creacion de quipo local
-    public static ArrayList<String> listaEquipoLocal(String nombreArchivo){
+    public static ArrayList<String> listaEquipoLocal(String nombreArchivo) {
         ArrayList<String> grupoEquipo = new ArrayList<>();
-        try(BufferedReader bf = new BufferedReader(new FileReader(nombreArchivo))){
+        try ( BufferedReader bf = new BufferedReader(new FileReader(nombreArchivo))) {
             String linea;
-            while((linea=bf.readLine())!=null){
+            while ((linea = bf.readLine()) != null) {
                 String equipo[] = linea.split("\\|");
-                if(!(grupoEquipo.contains(equipo[5])) && !"Home Team Name".equals(equipo[5])){
+                if (!(grupoEquipo.contains(equipo[5])) && !"Home Team Name".equals(equipo[5])) {
                     String equipoconresultados = equipo[5];
 //                    System.out.println(Arrays.toString(equipoconresultados));
-                    
+
                     grupoEquipo.add(equipoconresultados);
-              
+
                 }
-                
+
             }
-            
-        }catch(FileNotFoundException fnfe){
+
+        } catch (FileNotFoundException fnfe) {
             System.out.println("Archivo no encontrado");
-        }catch(IOException ioe){
+        } catch (IOException ioe) {
             System.out.println("Error de programa");
         }
-        
+
         return grupoEquipo;
     }
-    
+
     //ListaEquipoVisitante
-    public static ArrayList<String> listaEquipoVisitante(String nombreArchivo){
+    public static ArrayList<String> listaEquipoVisitante(String nombreArchivo) {
         ArrayList<String> grupoEquipo = new ArrayList<>();
-        try(BufferedReader bf = new BufferedReader(new FileReader(nombreArchivo))){
+        try ( BufferedReader bf = new BufferedReader(new FileReader(nombreArchivo))) {
             String linea;
-            while((linea=bf.readLine())!=null){
+            while ((linea = bf.readLine()) != null) {
                 String datos[] = linea.split("\\|");
-                if(!(grupoEquipo.contains(datos[8])) && !"Away Team Name".equals(datos[8])){
-                    String equipoconresultados =datos[8];
+                if (!(grupoEquipo.contains(datos[8])) && !"Away Team Name".equals(datos[8])) {
+                    String equipoconresultados = datos[8];
 //                    System.out.println(Arrays.toString(equipoconresultados));
-                    
+
                     grupoEquipo.add(equipoconresultados);
-              
+
                 }
-                
+
             }
-            
-        }catch(FileNotFoundException fnfe){
+
+        } catch (FileNotFoundException fnfe) {
             System.out.println("Archivo no encontrado");
-        }catch(IOException ioe){
+        } catch (IOException ioe) {
             System.out.println("Error de programa");
         }
-        
+
         return grupoEquipo;
     }
+
+    //ListaConstructor
+    public static ArrayList<Partido> listaPartido(String archivo) {
+        ArrayList<Partido> listaP = new ArrayList<>();
+        try ( BufferedReader bf = new BufferedReader(new FileReader(archivo))) {
+            String linea = null;
+            while ((linea = bf.readLine()) != null) {
+                String datos[] = linea.split("\\|");
+//                if(!(listaP.contains(datos[8])) && !"Away Team Name".equals(datos[8])){
+//                                    }
+                listaP.add(new Partido(datos[0].trim(), datos[1].trim(), datos[2].trim(), datos[3].trim(), datos[4].trim(), datos[5].trim(), datos[6].trim(), datos[7].trim(), datos[8].trim(), datos[9].trim(), datos[10].trim(), datos[11].trim(), datos[12].trim(), datos[13].trim(), datos[14].trim(), datos[15].trim(), datos[16].trim(), datos[17].trim(), datos[18].trim(), datos[19].trim()));
+
+            }
+        } catch (FileNotFoundException fnfe) {
+
+        } catch (IOException ioe) {
+
+        }
+        listaP.remove(0);
+        return listaP;
+
+    }
+
     
-    
-    
-//    public static void main(String[] args) {
-//        System.out.println(Partido.listaFase("WorldCupMatchesBrasil2014.csv"));
-//        System.out.println(Partido.listaGrupos("WorldCupMatchesBrasil2014.csv"));
-//        System.out.println(Partido.listaEquipoLocal("WorldCupMatchesBrasil2014.csv"));
-//        System.out.println(Partido.listaEquipoVisitante("WorldCupMatchesBrasil2014.csv"));
-//        
+
+//    @Override
+//    public int compareTo(Partido o) {
+//        if (this.getStage() > o.getStage()) {
+//            return 1;
+//
+//        } else if (this.getStage() < o.getStage()) {
+//            return -1;
+//        } else {
+//            return 0;
+//        }
 //    }
-  
-//    }
+
 }
--- Proyecto_G7/src/main/java/com/mycompany/poroyecto_prueba/ConsultaPartidoController.java
+++ Proyecto_G7/src/main/java/com/mycompany/poroyecto_prueba/ConsultaPartidoController.java
@@ -6,20 +6,36 @@ package com.mycompany.poroyecto_prueba;
 
 import com.mycompany.model.Partido;
 import java.net.URL;
+//import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+//import java.util.Calendar;
+//import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.Set;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
+import javafx.scene.control.Button;
+
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 /**
- * Clase de controlador FXML ConsultaPartidoController que pertenece al
- paquete com.mycompany.poroyecto_prueba e implementa Initializable
+ * FXML Controller class
  *
- * @author Michael Estrada
- * @author Carlos Tingo
+ * @author USER
  */
 public class ConsultaPartidoController implements Initializable {
+
+    private final ArrayList<Partido> listPartido = Partido.listaPartido("WorldCupMatchesBrasil2014.csv");
 
     @FXML
     private ComboBox<String> faseCmb;
@@ -29,24 +45,200 @@ public class ConsultaPartidoController implements Initializable {
     private ComboBox<String> gruposCmb;
     @FXML
     private ComboBox<String> Eq2Cmb;
+    @FXML
+    private Button btnConsultar;
 
     /**
-     * Método sobreescrito que inicializa la clase de controlador.
-     * @param url
-     * @param rb
+     * Initializes the controller class.
      */
+    @FXML
+    private HBox contenedorResultadoPartido;
+    @FXML
+    private HBox contenedorFecha;
+    @FXML
+    private HBox contenedorUltimo;
+    @FXML
+    private VBox contenedorDatos;
+    @FXML
+    private VBox contenedorResultadoFinal;
+    @FXML
+    private VBox contenedorBotones;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        ArrayList<String> listG = Partido.listaGrupos("WorldCupMatchesBrasil2014.csv");
-        ArrayList<String> listF = Partido.listaFase("WorldCupMatchesBrasil2014.csv");
-        ArrayList<String> listEquipLocal = Partido.listaEquipoLocal("WorldCupMatchesBrasil2014.csv");
-        ArrayList<String> listEquipoVisit = Partido.listaEquipoVisitante("WorldCupMatchesBrasil2014.csv");
-        //setear CmbBox
-        gruposCmb.getItems().setAll(listG);
-        faseCmb.getItems().setAll(listF);
-        Eq1Cmb.getItems().setAll(listEquipLocal);
-        Eq2Cmb.getItems().setAll(listEquipoVisit);
-        
+        // TODO
+        cargarFase();
+        cargarGrupos();
+
     }
 
+    /**
+     * Este metodo se encarga de cargar el ComboBox Fase
+     */
+    private void cargarFase() {
+        faseCmb.getItems().addAll(Partido.listaFase("WorldCupMatchesBrasil2014.csv"));
+        faseCmb.setOnAction(new EventHandler() {
+            @Override
+            public void handle(Event t) {
+                String s = faseCmb.getSelectionModel().getSelectedItem();
+                //GRUPOS 
+
+                if ("Group".equals(s)) {
+                    gruposCmb.setDisable(false);
+                    // LOS QUE NO SON DE GRUPOS
+                } else {
+
+                    String sg = faseCmb.getSelectionModel().getSelectedItem();
+                    gruposCmb.setDisable(true);
+
+                    cargarEquipos(sg);
+                    seleccionarEquipo(faseCmb, Eq1Cmb, Eq2Cmb);
+                }
+            }
+        });
+    }
+
+    /**
+     * Este metodo carga los grupos al ComboBox cmbGrupos
+     */
+    private void cargarGrupos() {
+
+        gruposCmb.getItems().addAll(Partido.listaGrupos("WorldCupMatchesBrasil2014.csv"));
+        gruposCmb.setOnAction(new EventHandler() {
+            @Override
+            public void handle(Event t) {
+                String sg = gruposCmb.getSelectionModel().getSelectedItem();
+                System.out.println(sg);
+                cargarEquipos(sg);
+                seleccionarEquipo(gruposCmb, Eq1Cmb, Eq2Cmb);
+
+            }
+        });
+
+    }
+
+    /**
+     * Este metodo carga los equipos
+     *
+     * @param sg recibe como parametro el tipo de seleccion para cada caso ya
+     * sea si se busca por grupo o Fase
+     */
+    private void cargarEquipos(String sg) {
+//        String sg = gruposCmb.getSelectionModel().getSelectedItem();
+//        System.out.println(sg);
+        Eq1Cmb.getItems().clear();
+        Eq2Cmb.getItems().clear();
+        for (Partido l : listPartido) {
+            if (sg.equals(l.getStage())) {
+
+                if (!(Eq1Cmb.getItems().contains(l.getHomeTN())) && !(Eq2Cmb.getItems().contains(l.getAwayTN()))) {
+
+                    Eq1Cmb.getItems().addAll(l.getHomeTN());
+                    Eq2Cmb.getItems().addAll(l.getAwayTN());
+
+                }
+
+            }
+
+        }
+    }
+
+    private void seleccionarEquipo(ComboBox fase, ComboBox eq1, ComboBox eq2) {
+        btnConsultar.setOnAction(new EventHandler< ActionEvent>() {
+            @Override
+            public void handle(ActionEvent t) {
+
+                Partido partidoSeleccionado = null;
+                Label txtConsultaPartido = new Label();
+
+                for (Partido r : listPartido) {
+                    contenedorResultadoPartido.getChildren().clear();
+
+                    if (fase.getSelectionModel().getSelectedItem().equals(r.getStage()) && eq1.getSelectionModel().getSelectedItem().equals(r.getHomeTN()) && eq2.getSelectionModel().getSelectedItem().equals(r.getAwayTN())) {
+                        partidoSeleccionado = r;
+
+                    } else {
+                        contenedorFecha.getChildren().clear();
+                        contenedorUltimo.getChildren().clear();
+                        contenedorDatos.getChildren().clear();
+                        contenedorBotones.getChildren().clear();
+                        contenedorResultadoPartido.getChildren().removeAll(txtConsultaPartido);
+                        txtConsultaPartido.setText("Partido no encontrado");
+                        contenedorResultadoPartido.getChildren().add(txtConsultaPartido);
+                    }
+
+                }
+
+                contenedorResultadoPartido.getChildren().clear();
+                txtConsultaPartido.setText("Resultado del partido");
+                txtConsultaPartido.setFont(Font.font("Arial", FontWeight.BOLD, 13));
+                contenedorResultadoPartido.getChildren().add(txtConsultaPartido);
+                try {
+                    if (partidoSeleccionado != null) {
+                        String[] dateTime = partidoSeleccionado.getDateTime().trim().split("-");
+                        contenedorFecha.getChildren().add(new Label(dateTime[0].trim()));
+                    }
+
+                } catch (NullPointerException npe) {
+
+                }
+            }
+        });
+
+//        DateTimeFormatter esDateFormatLargo = DateTimeFormatter.ofPattern("EEEE, dd 'de' MMMM 'de' yyyy '").withLocale(new Locale("es", "ES"));
+    }
+
+   
+
+//        if(){
+    @FXML
+    private void consultar(ActionEvent event) {
+
+    }
 }
+
+//    @FXML
+//    private void selectFase(ActionEvent e) {
+//        String s = faseCmb.getSelectionModel().getSelectedItem();
+//        System.out.println(s);
+//        if (!"Group".equals(s)) {
+//            gruposCmb.getItems().clear();
+////            Eq1Cmb.getItems().addAll(Partido.listaEquipoLocal("WorldCupMatchesBrasil2014.csv"));
+////            Eq2Cmb.getItems().addAll(Partido.listaEquipoVisitante("WorldCupMatchesBrasil2014.csv"));
+//        } else {
+//            gruposCmb.getItems().addAll(Partido.listaGrupos("WorldCupMatchesBrasil2014.csv"));
+//
+//        }
+//
+//    }
+//// "WorldCupMatchesBrasil2014.csv"
+//
+//    @FXML
+//    private void selectGrupos(ActionEvent e) {
+//        String sg = gruposCmb.getSelectionModel().getSelectedItem();
+//        //Grupo A
+//        System.out.println(sg);
+//        ArrayList<Partido> listPartido = Partido.listaPartido("WorldCupMatchesBrasil2014.csv");
+//        Eq1Cmb.getItems().clear();
+//        for (Partido l : listPartido) {
+//            System.out.println(l.getStage() + " - " + l.getHomeTN());
+//            if (sg.equals(l.getStage())) {
+//                if(!(Eq1Cmb.getItems().contains(l.getHomeTN()))){
+//                    Eq1Cmb.getItems().addAll(l.getHomeTN());
+//                    
+//                }
+//                
+//
+//            } else {
+////                   System.out.println(sg);
+////                   System.out.println(l.getStage());
+////                   System.out.println(l.getHomeTN());
+//            }
+//        }
+//    }
+//    //comparar equipos
+//    private void compararEquipos(){
+//        
+//        
+//    }
+
